0105d4e818131771f370abfff32c6f6d022f7908
Tomas Di Vito <tomasdv2@gmail.com>
2017-01-19 21:26:10 -0300

    Change a typo in a function

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const SweetAlert = cn.pedant.SweetAlert;
 
 //for context you can send app.android.context with the require('application')
 var simpleAlert = function(title, context) {
-  var dialog = new SweetAlert.SweetAlertDialog(context).setTitle(title).show();
+  var dialog = new SweetAlert.SweetAlertDialog(context).setTitleText(title).show();
 }
 exports.simpleAlert = simpleAlert;
