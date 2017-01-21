131765485e07a656165495eeaeee703c7e14fa1e
Tomas Di Vito <tomasdv2@gmail.com>
2017-01-20 23:10:29 -0300

    added and changed functions and modified the README

--- README.md
+++ README.md
@@ -6,10 +6,13 @@ Fill in a little about your plugin!
 ## License
 This plugin is licensed under the MITlicense by Tomas
 
+## Based on
+Android: sweet-alert-dialog by @pedant
+iOS: (wip) SweetAlert-iOS by @codestergit
+
 ## Current State
 For now you can do not so many things but i will be adding new stuff in the next days.
 
-Still not thinking into making this for iOS for now.
 
 ## Installation
 To install type
@@ -28,10 +31,32 @@ var app = require('application');
 var SweetAlert = require('ns-sweet-alert');
 
 var alertNice = function() {
-  SweetAlert.simpleAlert("Hey this is a pretty alert for NS!", app.android.foregroundActivity);
+  SweetAlert.simpleAlert(app.android.foregroundActivity).setTitleText("hey!").setContentText("this is a sweetmessage");
 }
-exports.push = push;
+
+var inError = function(args) {
+  SweetAlert.errorAlert(app.android.foregroundActivity).setTitleText("Oops!").setContentText(args);
+}
+
+exports.alertNice = alertNice;
+exports.inError = inError;
 ```
+
+## Current Types
+
+``` js
+simpleAlert(context)
+errorAlert(context)
+warningAlert(context)
+successAlert(context)
+
+//you can use various methods with the dialog result
+.setTitleText
+.setContentText
+.setConfirmText
+```
+
+I will be continue adding more things, like iOS support sometime this week!
 
 And that's it!
 
--- index.js
+++ index.js
@@ -8,8 +8,47 @@ var app = require('application');
 
 const SweetAlert = cn.pedant.SweetAlert;
 
-//for context you can send app.android.foregroundActivity with the require('application')
-var simpleAlert = function(title, context) {
-  var dialog = new SweetAlert.SweetAlertDialog(context).setTitleText(title).show();
+/***
+Expects the context and you receive a SweetAlertDialog to do something like
+
+dialog.setTitleText("hello").setContentText("this is a sweet-message").show();
+***/
+var simpleAlert = function(context) {
+  var dialog = new SweetAlert.SweetAlertDialog(context);
+  return dialog;
 }
+
+/***
+Expects the context and you receive a SweetAlert dialog error type
+
+works exactly like simpleAlert()
+***/
+var errorAlert = function(context) {
+  var dialog = new SweetAlert.SweetAlertDialog(context, SweetAlert.SweetAlertDialog.ERROR_TYPE);
+  return dialog;
+}
+
+/***
+Expects the context and you receive a SweetAlert dialog Warning Type
+
+usage:
+dialog.setTitleText("hello").setContentText("this is a sweet-message").setConfirmText("yes, delete it").show();
+***/
+var warningAlert = function(context) {
+  var dialog = new SweetAlert.SweetAlertDialog(context, SweetAlert.SweetAlertDialog.WARNING_TYPE);
+  return dialog;
+}
+
+/***
+Expects the context and you receive a SweetAlert dialog success type
+
+works exactly like simpleAlert()
+***/
+var successAlert = function(context) {
+  var dialog = new SweetAlert.SweetAlertDialog(context, SweetAlert.SweetAlertDialog.SUCCESS_TYPE);
+  return dialog;
+}
+
+
+
 exports.simpleAlert = simpleAlert;
